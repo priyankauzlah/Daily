9e866ab30141be8d4d051203d41046b00bbd018c
priyanka uzlah <priyankauzlah@gmail.com>
2021-02-22 20:49:15 +0700

    Can get name and profile picture

--- app/src/main/java/com/uzlahpri/newsappp/activity/ProfileActivity.kt
+++ app/src/main/java/com/uzlahpri/newsappp/activity/ProfileActivity.kt
@@ -5,16 +5,21 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
 import com.uzlahpri.newsappp.R
 import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity(), View.OnClickListener {
 
+    var refUsers: DatabaseReference? = null
+    var firebaseUser: FirebaseUser? = null
+
     companion object {
         fun getLaunchService(from: Context) = Intent(from, ProfileActivity::class.java).apply {
             addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-
         }
     }
 
@@ -22,12 +27,35 @@ class ProfileActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
         supportActionBar?.hide()
-        btn_logout.setOnClickListener(this)
+        tv_logout.setOnClickListener(this)
+        firebaseUser = FirebaseAuth.getInstance().currentUser
+        refUsers = FirebaseDatabase.getInstance().reference.child(firebaseUser!!.uid)
+
+        refUsers!!.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (p0 in snapshot.children) {
+                    val name = snapshot.child("fullName").value.toString()
+                    val email = snapshot.child("email").value.toString()
+                    val photo = snapshot.child("photo").value.toString()
+                    tv_name_profile.text = name
+                    tv_email_profile.text = email
+                    Glide.with(this@ProfileActivity).load(photo).into(iv_profile)
+
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
+
 
     override fun onClick(p0: View) {
         when (p0.id) {
-            R.id.btn_logout -> logOut()
+            R.id.tv_logout -> logOut()
+            R.id.ib_back_profile -> startActivity(Intent(MainActivity.getLaunchService(this)))
         }
     }
 
